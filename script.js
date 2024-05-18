358acd16083a88094a3b275dddf38f34fadc3e7b
ilman677 <harunarma350@gmail.com>
2024-05-18 18:43:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Terima kasih, ${name}! Pesan Anda telah terkirim.`);
+
+    document.getElementById('contactForm').reset();
+});
+
+document.getElementById('fullscreenBtn').addEventListener('click', function() {
+    const videoElement = document.getElementById('myVideo');
+    if (videoElement.requestFullscreen) {
+        videoElement.requestFullscreen();
+    } else if (videoElement.mozRequestFullScreen) { // Firefox
+        videoElement.mozRequestFullScreen();
+    } else if (videoElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        videoElement.webkitRequestFullscreen();
+    } else if (videoElement.msRequestFullscreen) { // IE/Edge
+        videoElement.msRequestFullscreen();
+    }
+});
